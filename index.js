bbe77c000315a40cdb768dfde68c1b3f4009e0d6
zeroman1444 <josericardochan@hotmail.com>
2019-08-04 22:28:32 -0500

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const multer = require('multer');
+
 const uuid = require('uuid/v4');
 const { format } = require('timeago.js');
 
@@ -17,13 +17,6 @@ app.set('view engine', 'ejs');
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended : false}));
-const storage = multer.diskStorage({
-    destination: path.join(__dirname, 'public/img/uploads'),                                            
-    filename: (req, file, cb, filename) => {
-      cb(null, uuid() + path.extname(file.originalname));
-    }
-});
-app.use(multer({storage: storage}).single('image'));
 
 //Global variables
 app.use((req, res, next) => {
